1d40867a1aa763408182a0eca0e46475a66e7224
Bálint Csertán <csertanb@gmail.com>
2024-03-11 08:35:48 +0100

    add inquirer

--- calculator.js
+++ calculator.js
@@ -1,44 +1,48 @@
 import chalk from 'chalk';
-import readline from 'readline/promises';
+import { input, select } from '@inquirer/prompts';
 import { add, subtract, multiply, divide } from './operations.js';
 
-let rl = null;
 let op = null, op1 = null, op2 = null;
 
 export default async function calculator() {
-    initInput();
     await getInput();
     await calculate();
-    await rl.close();
 }
-
-
-function initInput() {
-    rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    })
-};
 
 function isNumber(number) {
     return typeof number === 'number' && !isNaN(number);
 }
 
 async function getOperation() {
-    const operation = await rl.question('Which operation would you like to perform? (+, -, *, /): ');
-
-    let operations = ["+", "-", "*", "/"];
-
-    if (operations.includes(operation)) {
-        return operation;
-    } else {
-        console.log("That is not a possible operation!");
-        return getOperation();
-    }
+    return await select({
+        message: 'Which operation would you like to perform?',
+        choices: [
+            {
+                name: '+',
+                value: '+',
+                description: 'plus operator',
+            },
+            {
+                name: '-',
+                value: '-',
+                description: 'minus operator',
+            },
+            {
+                name: '*',
+                value: '*',
+                description: 'multiply operator'
+            },
+            {
+                name: '/',
+                value: '/',
+                description: 'division operator'
+            },
+        ],
+    });
 }
 
 async function getNumber(message) {
-    const op = parseInt(await rl.question(message), 10);
+    const op = parseInt(await input({ message }), 10);
     if (!isNumber(op)) {
         console.log("The given input is not a number! Try again...");
         return getNumber(message);
